2dbca7798b4c79d5c7ee9fd4700bcd9565c86eab
Shuma <kasai.0124.sss@gmail.com>
2022-11-30 18:12:36 +0900

    good

--- problem2.py
+++ problem2.py
@@ -1,4 +1,5 @@
 import re
+import collections
 
 # 読み込み
 with open(r"data\new_test.txt") as reader:
@@ -7,10 +8,9 @@ with open(r"data\new_test.txt") as reader:
 # 置換
 content = content.lower()
 content = re.sub(r"[^a-z]","",content)
-abc_dict = {i: content.count(i) for i in content}
-#print(abc_dict)
+c = collections.Counter(content)
+
 # 書き出し
 #with open(r"data\count_test.txt", 'w') as writer:
 #    writer.write(abc_dict)
-abc_dict = sorted(abc_dict.items())
-print(abc_dict)
+print(c.most_common())
