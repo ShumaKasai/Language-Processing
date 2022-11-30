718a3a0f3b9850eee4c62b9c18a7841023338c75
Shuma <kasai.0124.sss@gmail.com>
2022-11-30 18:45:50 +0900

    smart

--- problem2.py
+++ problem2.py
@@ -9,7 +9,3 @@ content = content.lower()
 content = re.sub(r"[^a-z]","",content)
 abc_count = collections.Counter(content)
 print(abc_count)
-
-# 書き出し
-#with open(r"data\count_test.txt", 'w') as writer:
-#    writer.write(abc_dict)
